6d6342efc4dd92cd730a60dda40765d54f14edab
hwolfinger <haroldwolfingerIT@gmail.com>
2022-10-12 17:15:50 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const navbarEl = document.querySelector(".navbar");
+
+const bottomContainerEl = document.querySelector(".bottom-container");
+
+window.addEventListener("scroll", ()=> {
+    console.log("scrolled");
+    console.log(window.scrollY);
+});
+
